580a2306994da4763e094e3f6a28b716cb67d413
Zislw <w05567716@gmail.com>
2022-07-03 22:37:57 +0300

    Updates

---
+++ precache-manifest.996220c5094d8716c4ff9115bb93a4b7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "879b262bff41560a98e1",
+    "url": "/good_react_project28.06/static/css/main.ab6c649d.chunk.css"
+  },
+  {
+    "revision": "879b262bff41560a98e1",
+    "url": "/good_react_project28.06/static/js/main.879b262b.chunk.js"
+  },
+  {
+    "revision": "0d630a3e1f21db7817dc",
+    "url": "/good_react_project28.06/static/js/1.0d630a3e.chunk.js"
+  },
+  {
+    "revision": "7d89464741f2193926da",
+    "url": "/good_react_project28.06/static/js/2.7d894647.chunk.js"
+  },
+  {
+    "revision": "f5ab42eb8f5f85d74a98",
+    "url": "/good_react_project28.06/static/js/runtime~main.f5ab42eb.js"
+  },
+  {
+    "revision": "20ae8df0a2d536bd5aea787f81dcac7a",
+    "url": "/good_react_project28.06/index.html"
+  }
+];
